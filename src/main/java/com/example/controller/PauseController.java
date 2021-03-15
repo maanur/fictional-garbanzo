5b8739c6222f4fe909b3c7b7a26099f897db9d92
maanur <onmyouji777@gmail.com>
2021-03-15 22:08:09 +0300

    Switch to MySQL

--- src/main/java/com/example/controller/PauseController.java
+++ src/main/java/com/example/controller/PauseController.java
@@ -2,7 +2,6 @@ package com.example.controller;
 
 import com.example.config.PauseHealthIndicator;
 import io.micronaut.http.annotation.Controller;
-import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 
 import javax.inject.Inject;
